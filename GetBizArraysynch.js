80521a9ca09b8540dc342470f6157df32f0e8972
David4Mobee <33639745+David4Mobee@users.noreply.github.com>
2017-11-15 12:46:15 -0500

    Add files via upload
    Automated script - first attempt

---
+++ GetBizArraysynch.js
@@ -0,0 +1,27 @@
+var Yelp = require('yelpv3');
+
+var yelp = new Yelp({
+  app_id: 'fUzxW_17GY1tDUv-y4KkeQ',
+  app_secret: 's0oShPo6wnabFlaBAGgJ36tK3Wg5IIXI9GNImwhnSCAIkyDqauvU7s5f78AIrEPd'
+});
+
+var fs = require('fs');
+
+var i = require('pno');
+
+// https://www.yelp.com/developers/documentation/v3/business_search_phone
+yelp.phoneSearch({phone: 'pno'})
+.then(function (data) { console.log(data); })
+.catch(function (err) { console.error(err);});
+
+
+var arrayNumbers = fs.readFileSync('phoneNumber.txt').toString().split("\n");
+for(i in arrayNumbers) {
+
+// at this point each phone number from the phoneNumber.txt is an element of the array
+
+//the only thing you have to do is assign the element to a variable and run your script
+
+
+   
+}
